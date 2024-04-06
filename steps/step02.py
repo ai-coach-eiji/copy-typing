93cee04af3feccea00521135832137ccfa225e62
eiji <soyeiji1220@gmail.com>
2024-04-06 11:59:32 +0900

    Added forward method to Fucntion class

--- steps/step02.py
+++ steps/step02.py
@@ -1,13 +1,16 @@
 class Variable:
     def __init__(self, data):
         self.data = data
-        
+
 class Function:
     def __call__(self, input):
         x = input.data
-        y = x ** 2
+        y = self.forward(x)
         output = Variable(y)
         return output
+
+    def forward(self, x):
+        raise NotImplementedError()
 
 import numpy as np
 
