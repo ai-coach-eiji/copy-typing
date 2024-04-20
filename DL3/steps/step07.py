a357d507adbe34cdd6dee9cb1c818ba079dbf085
eiji <soyeiji1220@gmail.com>
2024-04-20 13:39:04 +0900

    Evaluated the backward method

--- DL3/steps/step07.py
+++ DL3/steps/step07.py
@@ -66,3 +66,16 @@ assert y.creator.input.creator == B
 assert y.creator.input.creator.input == a
 assert y.creator.input.creator.input.creator == A
 assert y.creator.input.creator.input.creator.input == x
+
+y.grad = np.array(1.0)
+
+C = y.creator
+b = C.input
+b.grad = C.backward(y.grad)
+B = b.creator
+a = B.input
+a.grad = B.backward(b.grad)
+A = a.creator
+x = A.input
+x.grad = A.backward(a.grad)
+print("x grad: ", x.grad)
