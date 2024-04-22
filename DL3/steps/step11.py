19ee037eee713d10c6c939b4d68a7d71fccf0278
eiji <soyeiji1220@gmail.com>
2024-04-22 23:21:56 +0900

    Modified Function class for varargs

---
+++ DL3/steps/step11.py
@@ -0,0 +1,17 @@
+class Function:
+    def __call__(self, inputs):
+        xs = [x.data for x in inputs]
+        ys = self.forward(xs)
+        outputs = [Variable(as_array(y)) for y in ys]
+
+        for output in outputs:
+            output.set_creator(self)
+        self.inputs = inputs
+        self.outputs = outputs
+        return outputs
+    
+    def forward(self, xs):
+        raise NotImplementedError()
+
+    def backward(self, gys):
+        raise NotImplementedError()
